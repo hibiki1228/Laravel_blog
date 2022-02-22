2d041d6b575147c49bb162989b6688adbaca4c92
hibiki1228 <devkimadore1228@gmail.com>
2022-02-23 00:10:29 +0900

    add migration and seeder

---
+++ my_blog/database/factories/BlogFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Inquiry>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word,
+            'mainScript' => $this->faker->text,
+            'category' => $this->faker->randomDigit,
+        ];
+    }
+}
