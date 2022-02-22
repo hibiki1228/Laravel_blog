5b2a75ca7029b61c4f51f6dc86366ef2702c79fe
hibiki1228 <devkimadore1228@gmail.com>
2022-02-22 21:56:33 +0900

    made first views

---
+++ my_blog/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <script src="{{ asset('js/app.js') }}" ></script> --}}
+    <title>Laravel-Movies</title>
+  </head>
+  <body>
+    <header class="w-100">
+      @include('header')
+    </header>
+    <main class="w-100 px-5 py-4 bg-light">
+      @yield('content')
+    </main>
+  </body>
+</html>
